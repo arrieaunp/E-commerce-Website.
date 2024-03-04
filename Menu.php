8d478907099cc9063b82ce4e1c2df61581b0a094
abb1 <siravit2580@gmail.com>
2024-03-04 12:04:14 +0700

    Search

--- Menu.php
+++ Menu.php
@@ -17,7 +17,7 @@ require_once "db_config.php";
   <header>
     <nav>
       <ul>
-        <li><a href="index.php">หน้าแรก</a></li>
+        <li><a href="Menu.php">หน้าแรก</a></li>
         <li><a href="Order_history.php">ประวัติการซื้อ</a></li>
         <li><a href="Cart.php">ตะกร้าสินค้า</a></li>
         <?php
@@ -55,38 +55,42 @@ require_once "db_config.php";
   <center>
 
     <h1>รายการสินค้า</h1>
-    <form method="GET" action="index.php">
+    <form method="GET" action="Menu.php">
       <input type="text" name="search" placeholder="ค้นหาสินค้า..." value="<?php echo isset($_GET['search']) ? htmlspecialchars($_GET['search']) : ''; ?>">
       <button type="submit">ค้นหา</button>
     </form>
     <form method="GET" action="Cart.php">
     <?php
 
-    if (isset($_GET['search'])) {
+    if(isset($_GET['search'])) {
       $search = mysqli_real_escape_string($conn, $_GET['search']);
       $query = "SELECT * FROM `Stock` WHERE `ProductName` LIKE '%$search%'";
+      $result = mysqli_query($conn, $query);
     } else {
-      $query = "SELECT * FROM `Stock`";
+      $result = mysqli_query($conn, "SELECT * FROM `Stock`");
     }
 
-    $cur = mysqli_query($conn, "SELECT * FROM `Stock` WHERE 1");
-    while ($row = mysqli_fetch_array($cur)) {
-      $name1 = $row["ProductCode"];
-      $name2 = $row["ProductName"];                
-      $name3 = $row["PricePerUnit"];
-      $name4 = $row["StockQty"];
-      $name6 = $row["ProductImg"];
+    if(mysqli_num_rows($result) > 0) {
+      while ($row = mysqli_fetch_array($result)) {
+          $name1 = $row["ProductCode"];
+          $name2 = $row["ProductName"];
+          $name3 = $row["PricePerUnit"];
+          $name4 = $row["StockQty"];
+          $name6 = $row["ProductImg"];
 
-      echo  "<div class='product-container'>",
-            "<div class='product-card'>",
-            "<img class='img' src='image/$name6'>",
-            "<p> <b>$name2</b></p>",
-            "<p><b> ราคา : </b>$name3 บาท</p>",
-            "<p><b>สินค้าคงเหลือ : </b>$name4 ชิ้น</p>",
-            "<button type='submit' class='button button1' value='$name1' name='selected'> Add To Cart </button>",
-            "</div>",
-            "</div>";
-    }
+          echo "<div class='product-container'>",
+                  "<div class='product-card'>",
+                  "<img class='img' src='image/$name6'>",
+                  "<p><b>$name2</b></p>",
+                  "<p><b> ราคา : </b>$name3 บาท</p>",
+                  "<p><b>สินค้าคงเหลือ : </b>$name4 ชิ้น</p>",
+                  "<button type='submit' class='button button1' value='$name1' name='selected'> Add To Cart </button>",
+                  "</div>",
+              "</div>";
+      }
+  } else {
+      echo '<div class="center">No product found</div>';
+  }
     ?>
   </center>
 
