91f4b596dd2b314ef0f2fc8fc1220aadd417a1de
ppearr <aunchisachanatipakul@MacBook-Air-khxng-ppearr.local>
2024-03-07 01:12:42 +0700

    income summary

--- AdminPage/Income_report.php
+++ AdminPage/Income_report.php
@@ -16,7 +16,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!doctype html>
 <html lang="en">
 <head>
-    <title>Income Report</title>
+    <title>Incom Summary Report</title>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
