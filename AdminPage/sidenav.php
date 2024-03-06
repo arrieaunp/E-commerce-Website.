aad6f61f13a3f7f764f28171f0717299b2ae3f01
ppearr <aunchisachanatipakul@MacBook-Air-khxng-ppearr.local>
2024-03-07 00:52:39 +0700

    seperate side nav

---
+++ AdminPage/sidenav.php
@@ -0,0 +1,10 @@
+<nav role='navigation'>
+  <ul class="main">
+    <li class="dashboard"><a href="Adminpage.php">Dashboard</a></li>
+    <li class="users"><a href="../Customer/Show_Customer.php">Customer</a></li>
+    <li class="write"><a href="../Stock/Show_Stock.php">Stock</a></li>
+    <li class="comments"><a href="Income_report.php">Income Report</a></li>
+    <li class="comments"><a href="ProductSales_report.php">Product Sales Report</a></li>
+
+  </ul>
+</nav>
